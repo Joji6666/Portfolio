b096f45e290206e38b71d3f37655972f2969bf89
mac <mac@macui-MacBook-Air.local>
2023-01-04 16:24:05 +0900

    헤더 풋터 아이콘 추가

--- components/footer.js
+++ components/footer.js
@@ -5,6 +5,15 @@ export default function Footer() {
     <>
       <footer className="text-gray-600 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="blue"
+          >
+            <path d="M24 10.935v2.131l-8 3.947v-2.23l5.64-2.783-5.64-2.79v-2.223l8 3.948zm-16 3.848l-5.64-2.783 5.64-2.79v-2.223l-8 3.948v2.131l8 3.947v-2.23zm7.047-10.783h-2.078l-4.011 16h2.073l4.016-16z" />
+          </svg>
           <Link href="/">
             <div
               className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
